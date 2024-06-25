cc0deaa8cb0c0fe26aff194645fc8a85e6e41851
Tomasz Klemczak <128645249+Tomczon@users.noreply.github.com>
2024-06-25 21:29:49 +0200

    The code
    uploading the code

---
+++ layouts/basic.vue
@@ -0,0 +1,96 @@
+
+<template style = "scroll-behavior = smooth;">
+  <body>
+    <div>
+      <header>
+          <img src="/Given_logo.png" alt="Logo" class="logo" />
+          <div class = "nav">
+            <button class="button1" onclick="document.getElementById('banner').scrollIntoView();">Home</button>
+            <button class="button1" onclick="document.getElementById('About').scrollIntoView();">About</button>
+            <button class="button1" onclick="document.getElementById('Members').scrollIntoView();">Members</button>
+            <button class="button1" onclick="document.getElementById('Music').scrollIntoView();">Music</button>
+            <button class="button1" onclick="document.getElementById('Contact').scrollIntoView();">Contact</button>
+        </div>
+      </header>
+      <slot />
+      <footer>
+        <p>&copy; 2024 Given. All rights reserved.</p>
+      </footer>
+    </div>
+  </body>
+  </template>
+  
+  <style scoped>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+  body{
+    background-image: -webkit-linear-gradient(top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(public/group_photo.png);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover; 
+    background-position: top center;
+    scroll-behavior: smooth;
+    }
+
+  header {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    background: #333;
+    color: #fff;
+    position: fixed;
+    width: 98.5%;
+    scroll-behavior: smooth;
+  }
+  
+  nav {
+    display: flex;
+    align-items: center;
+  }
+  
+  .logo {
+    max-height: 50px;
+    margin-right: 20px;
+    margin-left: 10px;
+    padding: 10px 10px;
+  } 
+  
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  
+  footer {
+    text-align: center;
+    padding: 10px 0;
+    background: #333;
+    color: #fff;
+    position: fixed;
+    width: 98.5%;
+    bottom: 0;
+  }
+
+  .button1
+  {
+    background-color: #333;
+    border: none;
+    padding: 25px 25px;
+    font-size: 1vw; 
+    color: white;
+    height: 100%;
+    transition: background-color 0.3s ease;
+    cursor: pointer;
+
+  }
+
+  button:hover {
+    color: #923131;
+    background-color: #292828;
+  }
+  </style>
---
+++ pages/home.vue
@@ -0,0 +1,249 @@
+<script>
+definePageMeta({
+  layout: 'basic',
+})
+
+</script>
+
+<template>
+    <div>
+        <section id="banner">
+        <div class="inner">
+            <h2>CAN'T SAY GOODBYE, I'M STILL<br/>
+                DRIFTING WITH YOUR ECHOES</h2>
+                <Img src = "/Given_logo.png"></Img>
+        </div>
+    </section>
+
+    <section id="content">
+        <div id="About" class="content-section" >
+            <h1>We are Given!</h1>
+            <div class = "desc">
+                <Img class = "img2"src = "/group.jpg"></Img>  
+                <div class = "column">
+                <p class = "text">Given (formely The Seasons) is more than just a band; we're a group of friends brought together by fate and a shared love for music. Our journey started in a seemingly ordinary way, but it's become something extraordinary.</p>
+
+                <p class = "text">Our songs are born from our personal experiences and emotions, which I think is why they resonate so deeply with our fans. We don't just play music; we tell stories. Whether it's about love, loss, or finding yourself, there's always a piece of our hearts in every note and lyric.</p>
+
+                <p class = "text">Being in Given isn't just about performing; it's about the bonds we've formed and the way we’ve grown together. We've faced challenges, but those have only made us stronger. We're constantly learning from each other, pushing our boundaries, and creating something that feels truly special.</p>
+
+                <p class = "text">Thank you for supporting us and being part of our journey. We hope our music speaks to you the way it does to us.</p>
+
+                <p>-Mafuyu Sato</p>
+                </div>
+            </div>
+        </div>
+
+        <div id = "Members">
+            <h2>Members of Given</h2>
+        </div>
+
+        <div id="Mafuyu" class="content-section" >
+            <h1>Mafuyu Sato</h1>
+            <div class = "desc">
+                <Img class = "img2" src = "/mafuyu.png"></Img>  
+                <div class = "column">
+                <p class = "text2">Mafuyu Sato is the lead vocalist of Given, and he brings an emotional depth that defines the band's unique sound. His haunting, ethereal voice captivates listeners, while his poignant lyrics, drawn from personal experiences of love and loss, resonate deeply with fans. Mafuyu's vulnerability and sincerity not only enrich the band's music but also strengthen the bonds between the members. His journey of healing through music makes him the emotional core of Given, inspiring both his bandmates and their audience.</p>
+                </div>
+            </div>
+        </div> 
+
+        <div id="Ue" class="content-section" >
+            <h1>Ritsuka Uenoyama</h1>
+            <div class = "desc">
+                <Img class = "img2" src = "/ue.png"></Img>  
+                <div class = "column">
+                <p class = "text2">Ritsuka Uenoyama is the guitarist of Given and the driving force behind the band’s energetic sound. His exceptional guitar skills and deep passion for music reignite the band's creativity and push them to new heights. Ritsuka is also instrumental in guiding Mafuyu Sato, helping him find his voice and develop his songwriting. His dedication and musical talent form the backbone of Given, making him an essential and inspiring member of the group.</p>
+                </div>
+            </div>
+        </div> 
+
+        <div id="Haruki" class="content-section" >
+            <h1>Haruki Nakayama</h1>
+            <div class = "desc">
+                <Img class = "img2" src = "/haruki.png"></Img>  
+                <div class = "column">
+                <p class = "text2">Haruki Nakayama is the bassist of Given and the steady anchor of the band. As the oldest member, he brings maturity and stability to the group, often acting as the mediator and voice of reason. Haruki's smooth, intricate bass lines add depth and richness to their music, complementing the dynamic sounds of the guitar and vocals. His experience and calming presence help keep the band cohesive and focused, making him an indispensable part of Given.</p>
+                </div>
+            </div>
+        </div>
+        
+        <div id="Kaji" class="content-section" >
+            <h1>Akihiko Kaji</h1>
+            <div class = "desc">
+                <Img class = "img2" src = "/akihiko.png"></Img>  
+                <div class = "column">
+                <p class = "text2">Akihiko Kaji is the drummer of Given, known for his versatility and inventive style. His rhythmic precision and ability to experiment with different musical elements bring a unique and dynamic energy to the band’s performances. Akihiko's background in multiple instruments allows him to contribute creatively beyond drumming, adding layers and depth to their music. His adventurous approach and technical prowess make him a vital and exciting member of Given.</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="Music" class="content-section" >
+            <h1>Our Music</h1>
+            <div class = "music-column">
+
+                <iframe width="60%" height="752"
+                    src="https://www.youtube.com/embed/nAIPgTQ3QJ4">
+                </iframe>
+                <p style = " width: 80%; font-size: 30px; margin-top: 5vh;">"Winter Story" by Given is a poignant and emotionally charged song that captures the essence of longing and melancholy. The track features Mafuyu Sato's haunting vocals, which convey deep personal pain and introspection. With Ritsuka Uenoyama's evocative guitar riffs, Haruki Nakayama's grounding bass lines, and Akihiko Kaji's subtle yet impactful drumming, the song creates a powerful and immersive atmosphere. The lyrics and melody resonate with themes of love, loss, and the passage of time, making "Winter Story" a touching and memorable piece in Given's repertoire.</p>
+
+                <h1>This is our Spotify page!</h1>
+
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6LQYtKNp3ZsJ9xOzVVUj5j?utm_source=generator" width="80%" height="552" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                
+                <h1>Listen to our debut album "Gift"!</h1>
+
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/7wR9KWzI8aoR89ryUPDhmr?utm_source=generator" width="80%" height="552" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <p></p>
+            </div>
+        </div>
+
+        <div id="Contact" class="content-section" >
+            <h1>Contact Information</h1>
+            <div class = "info"> 
+                    <p class = "text">Managers number: +81 333 444 555 <br>
+                    e-mail: given@given.jp <br>
+                    If you have any more questions you can finds us at <a href="https://shinden.pl/series/54817-given">Given Anime</a></p>
+            </div>
+        </div>
+
+    </section>
+
+    </div>
+  </template>
+
+  <style scoped>
+
+a{
+    color: #923131;
+}
+
+.desc
+{
+    display: flex;
+    align-items: left;
+    justify-content: left;
+    text-align: left;
+    flex-direction: row;
+    gap:30px;
+    margin-bottom: 30px;
+    
+}
+
+.column
+{
+    display: flex;
+    align-items: left;
+    justify-content: left;
+    text-align: left;
+    flex-direction: column;
+}
+
+.music-column
+{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: left;
+    gap:px;
+}
+
+.info
+{
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: left;
+    text-align: left;
+    gap:20px;
+}
+
+.img2
+{
+    max-width: 600px;
+    max-height: 600px;
+    border-radius: 5%;
+    border-style: dashed;
+    border-color: rgb(87, 49, 49);
+    border-width: 5px;;
+    
+}
+
+#banner {
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+#banner .inner {
+    color: rgba(255, 255, 255, 0.7);
+    width: 90vh;
+    font-size: 30px;
+    font-family: Garamond, serif;
+    border-left: 3px solid rgba(255, 255, 255, 0.537);
+    border-right: 3px solid rgba(255, 255, 255, 0.537);
+}
+
+#content {
+    color:rgba(255, 255, 255, 0.7);
+    padding: 20px;
+    margin-left: 5%;
+    margin-right: 5%;
+    background-color:rgba(87, 49, 49, 0.8);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 95%;
+    border-radius: 10px;
+    margin-bottom: 100px;
+}
+.content-section {
+    margin-bottom: 20px;
+    max-width: 100%;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    width: 95%;
+    background-color: rgb(49, 49, 49);
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    border-style: dashed;
+    border-color: #3f3f3f;
+}
+#Members
+{
+    background-color:rgb(49, 49, 49);
+    width: 95%;
+    max-width: 100%;
+    text-align: center;
+    margin-bottom: 20px;
+    border-radius: 10px;
+    border-style: dashed;
+    border-color: #3f3f3f;
+    font-size: 3vh;
+    color:white;
+}
+
+h1
+{
+    margin-left: 1vh;
+    color:white;
+    font-size: 5vh;
+}
+
+.text
+{
+    font-size: 30px;
+}
+
+.text2
+{
+    font-size: 40px;
+}
+</style>
+  
